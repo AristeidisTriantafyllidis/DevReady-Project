6e648ccf2e706a1ced5703ab2a231a95e1a2c145
Aristeidis Triantafyllidis <tria.aris@gmail.com>
2026-05-24 15:51:12 +0300

    fail test for checking ci

--- src/App.test.js
+++ src/App.test.js
@@ -43,7 +43,7 @@ fetch.mockResolvedValue({
 })
   render(<App />)
   await waitFor(()=>{
-    expect(screen.getByText("Activity feed")).toBeInTheDocument();
+    expect(screen.getByText("Activity eeefeed")).toBeInTheDocument();
     expect(screen.getByText("+33 6 12 34 56 78")).toBeInTheDocument();
   })
 })
